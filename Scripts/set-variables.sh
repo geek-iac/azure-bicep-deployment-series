50934a9e077a18963e389d0ed5c08094846f438e
Iheanacho Chukwu <49342621+iheanacho-chukwu@users.noreply.github.com>
2024-09-10 18:07:05 +0100

    Update set-variables.sh

--- Scripts/set-variables.sh
+++ Scripts/set-variables.sh
@@ -1,5 +1,5 @@
-export AZP_URL='https://dev.azure.com/yourorganization/'
-export AZP_TOKEN='your_personal_access_token'
-export AZP_POOL='your agentpool name'
+export AZP_URL='https://dev.azure.com/nacho-chukwu/'
+export AZP_TOKEN='45dj5xojrxmuuu6g4cwa7y5dccsqyhlqkq3tcfxzehxzymnjwsmq'
+export AZP_POOL='linux-agent-pool'
 export AZP_AGENT_VERSION='3.243.1'
 export AGENT_ALLOW_RUNASROOT=0
